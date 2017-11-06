4bfb194ea5a918cc9a95a337eda4a41e6e940cca
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-11-06 15:47:42 +0530

    Add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BetFormComponent } from './components/bet-form/bet-form.component';
 import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+
+const appRoutes: Routes = [
+  { path: '', component: BetFormComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'scores', component: ScoreboardComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +23,8 @@ import { SignInComponent } from './components/sign-in/sign-in.component';
     SignInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
