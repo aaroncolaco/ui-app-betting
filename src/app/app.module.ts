e5fe220e187a06fd89451d90ae5de2ffaa78d115
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-11-06 15:36:54 +0530

    Create components: - Sign In - navbar - Betting form - Scoreboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BetFormComponent } from './components/bet-form/bet-form.component';
+import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BetFormComponent,
+    ScoreboardComponent,
+    NavbarComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/bet-form/bet-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bet-form',
+  templateUrl: './bet-form.component.html',
+  styleUrls: ['./bet-form.component.css']
+})
+export class BetFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
