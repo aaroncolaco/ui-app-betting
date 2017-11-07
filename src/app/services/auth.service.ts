c1526f5ab7aced6d2fc0b2f28480a309c98feb94
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-11-07 15:58:16 +0530

    Add methods to set & clear local auth tokens

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,12 +5,26 @@ export class AuthService {
 
   constructor() { }
 
+  clearBrowserToken(userName: string) {
+    localStorage.clear();
+  }
+
+  isUserLoggedIn(): boolean {
+    return localStorage.getItem('userName') ? true : false;
+  }
+
+  setBrowserToken(userName: string) {
+    localStorage.setItem('userName', userName);
+  }
+
+  // TODO: call API
   signUp(userName: string, password: string) {
     console.log('New User Sign Up', userName, password);
+    this.setBrowserToken(userName);
   }
 
   signIn(userName: string, password: string) {
     console.log('User Sign In', userName, password);
+    this.setBrowserToken(userName);
   }
-
 }
