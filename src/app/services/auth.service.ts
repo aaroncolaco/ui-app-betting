f60b28c9b72d0ad0aaaa735bfb170d909d48a848
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-11-08 10:52:54 +0530

    Add password hashing

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Md5 } from 'ts-md5/dist/md5';
 
 import { environment } from '../../environments/environment';
 
@@ -17,11 +18,16 @@ export class AuthService {
     localStorage.setItem('username', username);
   }
 
+  private hashPassword(password): string {
+    return <string>Md5.hashStr(password);
+  }
+
   isUserLoggedIn(): boolean {
     return localStorage.getItem('username') ? true : false;
   }
 
   signUp(username: string, password: string) {
+    password = this.hashPassword(password);
     this.http.post(environment.apiUrl + '/users/sign-up', { username, password })
       .subscribe(data => {
         if (data['status'] === 200) {
@@ -34,6 +40,7 @@ export class AuthService {
   }
 
   signIn(username: string, password: string) {
+    password = this.hashPassword(password);
     this.http.post(environment.apiUrl + '/users/sign-in', { username, password })
       .subscribe(data => {
         if (data['status'] === 200) {
