623fa0f1f7afb79cfce661373043ac7358faf039
Aaron Colaco <aaron_colaco@persistent.co.in>
2017-11-08 17:05:03 +0530

    Leaderboard layout

--- src/app/components/leaderboard/leaderboard.component.ts
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../../services/data.service';
+
+import { Leader } from '../../models/leader';
+
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaderboardComponent implements OnInit {
 
-  constructor() { }
+  leaders: Leader[];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.leaders = this.dataService.getLeaderBoard();
+    setInterval(() => {
+      this.leaders = this.dataService.getLeaderBoard();
+    }, 5000);
   }
 
 }
+
+
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Leader } from '../models/leader';
+
 @Injectable()
 export class DataService {
 
@@ -22,8 +24,16 @@ export class DataService {
     return 10; // TODO: API call to get balance
   }
 
-  getLeaderBoard(): Object {
-    return {}; // TODO: API call to get leaderboard
+  getLeaderBoard(): Leader[] {
+    const mock: Leader[] = [
+      { username: 'johndoe', balance: 100 },
+      { username: 'carol', balance: 96 },
+      { username: 'dave', balance: 80 },
+      { username: 'aliceparker', balance: 63 },
+      { username: 'jane', balance: 60 },
+      { username: 'steve', balance: 53 }
+    ];
+    return mock; // TODO: API call to get leaderboard
   }
 
 }
